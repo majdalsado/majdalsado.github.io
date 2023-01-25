f975f74c52a9ded1b0688a55870d549067a9899a
Majd Alsado <majdalsado@Ada-Majd-Alsado.local>
2023-01-25 00:48:33 -0800

    fixed errors

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,9 +1,7 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
-
-console.log(categories)
+const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}'{{ category_name | replace: " ", "_" }}': [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
